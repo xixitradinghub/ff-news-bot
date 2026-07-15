2dae6f88fb892f59b3f545e73601adb683c30bc0
xixitradinghub <xixitradinghub@gmail.com>
2026-07-15 15:09:07 +0800

    Update remind_before.py

--- remind_before.py
+++ remind_before.py
@@ -131,7 +131,7 @@ def build_message(triggered_events):
             current_time = time_str
             date_groups[-1]["time_blocks"].append([f"**{time_str}** - {event['title']}"])
         else:
-            padding = "\u00A0" * 10  # 不換行空格,對齊「HH:MM 」的寬度(反覆微調中)
+            padding = "\u00A0" * 12  # 不換行空格,對齊「HH:MM 」的寬度(反覆微調中)
             # 用 \- 跳脫橫線,避免 Discord 把「空格+-」誤判成條列清單符號
             date_groups[-1]["time_blocks"][-1].append(f"{padding}\\- {event['title']}")
 
